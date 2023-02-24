f6fbf5411fee200c2701eb3a277e4621c1ac75c3
M.Angel Juan <m.angel.juan@gmail.com>
2023-02-24 14:19:28 +0100

    Fix wrong path

--- _grains/tags_list.py
+++ _grains/tags_list.py
@@ -4,6 +4,6 @@ import os
 
 def main():
     try:
-        return {'tags':os.listdir('/etc/lliurex-auto-upgrade')}
+        return {'tags':os.listdir('/etc/lliurex-auto-upgrade/tags')}
     except Exception as e:
         return {'tags':[str(e)]}
