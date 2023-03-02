e76729451e441ad4bc62423009c78d991e7f16ea
M.Angel Juan <m.angel.juan@gmail.com>
2023-03-02 14:48:47 +0100

    fix llxversion

--- _grains/llxversion.py
+++ _grains/llxversion.py
@@ -3,7 +3,7 @@
 import subprocess
 
 def main():
-    version = subprocess.check_output(['/usr/bin/dpkg-query','-W','-f',"${Version}"]).decode().split('\n')
+    version = subprocess.check_output(['/usr/bin/dpkg-query','-W','-f',"${Version}", 'lliurex-version-timestamp']).decode().split('\n')
     if version:
-        return { 'llxversion' : version }
+        return { 'llxversion' : str(version) }
 
