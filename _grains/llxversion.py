4172b65b8618bcdf74bf5c266982d1f9038d0e23
M.Angel Juan <m.angel.juan@gmail.com>
2023-03-02 16:22:13 +0100

    wip

--- _grains/llxversion.py
+++ _grains/llxversion.py
@@ -6,5 +6,5 @@ def main():
     version = subprocess.check_output(['/usr/bin/dpkg-query','-W','-f',"${Version}", 'lliurex-version-timestamp']).decode()
     if isinstance(version,(list,dict)):
         version = str(version)
-    return str(version) 
+    return {'lver':str(version)}
 
