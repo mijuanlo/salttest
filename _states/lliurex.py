be2477a0e51cb0962866463848918a219248d841
M.Angel Juan <m.angel.juan@gmail.com>
2023-04-18 13:01:16 +0200

    wip

--- _states/lliurex.py
+++ _states/lliurex.py
@@ -2,8 +2,12 @@ import subprocess
 
 def desktop_present(name, user='all'):
     ret = {"name": name, "result": False, "comment": "", "changes": {}}
-    out = subprocess.check_output("cp /usr/share/applications/org.kde.kwrite.desktop /home/lliurex/Escriptori/",shell=True)
+    try:
+        out = subprocess.check_output("cp /usr/share/applications/org.kde.kwrite.desktop /home/lliurex/Escriptori/",shell=True,stderr=subprocess.STDOUT)
+        ret['result'] = True
+    except Exception as e:
+        out = str(e)
+        ret['result'] = False
     ret['comment'] = out
-    ret['result'] = True
     ret['changes'] = {"result": out}
     return ret
