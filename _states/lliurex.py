2555145dd1cd4808bc6aa20e16cc42198f6961cd
M.Angel Juan <m.angel.juan@gmail.com>
2023-04-18 14:37:58 +0200

    State for create desktop files

--- _states/lliurex.py
+++ _states/lliurex.py
@@ -4,6 +4,7 @@ import pwd
 
 def desktop_present(name, user='all'):
     ret = {"name": name, "result": False, "comment": "", "changes": {}}
+    created = []
     try:
         filename='/usr/share/applications/'+name
         if not os.path.isfile(filename):
@@ -17,7 +18,8 @@ def desktop_present(name, user='all'):
             dirname=dirname.strip()
             if not dirname or not os.path.isdir(dirname):
                 raise Exception('usererror: user {} with desktop {} not available'.format(user,dirname))
-            out = subprocess.check_output("cp {} {}".format(filename,dirname),shell=True,stderr=subprocess.STDOUT)
+            outmsg = subprocess.check_output("cp {} {}".format(filename,dirname),shell=True,stderr=subprocess.STDOUT)
+            created.append(user)
         else:
             out = []
             for us in (x.pw_name for x in pwd.getpwall()):
@@ -30,6 +32,7 @@ def desktop_present(name, user='all'):
                 if not dirname or not os.path.isdir(dirname):
                     continue
                 outmsg = subprocess.check_output("cp {} {}".format(filename,dirname),shell=True,stderr=subprocess.STDOUT)
+                created.append(us)
                 if outmsg:
                     out.append(outmsg)
             if out:
@@ -38,6 +41,9 @@ def desktop_present(name, user='all'):
     except Exception as e:
         out = str(e)
         ret['result'] = False
-    ret['comment'] = out
-    ret['changes'] = {"result": out}
+    if ret['result'] == True:
+        ret['comment'] = 'Created for user/s: {}'.format(','.join(created))
+    else:
+        ret['comment'] = 'Created for user/s: {}\nNote: {}'.format(','.join(created),out)
+    ret['changes'] = {"result": ret['comment']}
     return ret
