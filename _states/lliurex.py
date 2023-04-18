9634577f5768ff7760d88b0ab291d5156fad53ad
M.Angel Juan <m.angel.juan@gmail.com>
2023-04-18 12:52:24 +0200

    wip

--- _states/lliurex.py
+++ _states/lliurex.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def add_desktop(name, user='all'):
+def desktop_present(name, user='all'):
     ret = {"name": name, "result": False, "comment": "", "changes": {}}
     out = subprocess.check_output("cp /usr/share/applications/org.kde.kwrite.desktop /home/lliurex/Escriptori/",shell=True)
     ret['comment'] = out
