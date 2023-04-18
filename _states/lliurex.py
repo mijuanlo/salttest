0205ef025870d16a193e7ba617b9640eb616029e
M.Angel Juan <m.angel.juan@gmail.com>
2023-04-18 13:41:44 +0200

    wip

--- _states/lliurex.py
+++ _states/lliurex.py
@@ -1,13 +1,35 @@
 import subprocess
+import os.path
+import pwd
 
 def desktop_present(name, user='all'):
     ret = {"name": name, "result": False, "comment": "", "changes": {}}
     try:
-        out = subprocess.check_output("cp /usr/share/applications/org.kde.kwrite.desktop /home/lliurex/Escriptori/",shell=True,stderr=subprocess.STDOUT)
+        filename='/usr/share/applications/'+name
+        if not os.path.isfile(filename):
+            raise Exception('File {} not available'.format(filename))
+        if user != 'all':
+            dirname=subprocess.check_output("su {user} - -c 'xdg-user-dir DESKTOP'".format(user),shell=True,stderr=subprocess.STDOUT)
+            dirname=dirname.strip()
+            if not dirname or not os.path.isdir(dirname):
+                raise Exception('usererror: user {} with desktop {} not available'.format(user,dirname))
+            out = subprocess.check_output("cp {} {}".format(filename,dirname),shell=True,stderr=subprocess.STDOUT)
+        else:
+            out = []
+            for us in (for us in pwd.getpwall()):
+                dirname=subprocess.check_output("su {us} - -c 'xdg-user-dir DESKTOP'".format(us),shell=True,stderr=subprocess.STDOUT)
+                dirname=dirname.strip()
+                if not dirname or not os.path.isdir(dirname):
+                    continue
+                outmsg = subprocess.check_output("cp {} {}".format(filename,dirname),shell=True,stderr=subprocess.STDOUT)
+                if outmsg:
+                out.append(outmsg)
+            if out:
+                out = ','.join(out)
         ret['result'] = True
     except Exception as e:
         out = str(e)
         ret['result'] = False
-    ret['comment'] = name
+    ret['comment'] = out
     ret['changes'] = {"result": out}
     return ret
